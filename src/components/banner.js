f2a4edd28149c32fd13ea0c57114fe8f291afe7c
erdenerunl <erdenerunale@gmail.com>
2020-11-22 23:24:57 +0300

    banner was styled

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,27 +1,36 @@
 import React from "react"
-import { Container,Row,Col } from "reactstrap"
+import { Container, Row, Col } from "reactstrap"
 import HeaderStyles from "../components/header.module.scss"
-
+import "./header.module.scss"
 
 const Banner = () => {
-    return (
-        <div className={HeaderStyles.bannerDiv} >
-            <Container fluid className={HeaderStyles.bannerCon}>
-                <Row className={HeaderStyles.bannerRow}>
-                    <Col>
-                    <h1 className={HeaderStyles.heading1}> to the edges!</h1>
-                    </Col>
-                    <Col>
-                    <h3 className={HeaderStyles.heading3}>Everything to you need about climbing.</h3>
-                    </Col>
-                </Row>
-                <Row className={HeaderStyles.bannerRow}>
-                <div style={{width:"100px" ,height:"100px",backgroundColor:"aqua"}}></div>
-                </Row>  
-            </Container>
-            
-        </div>
-    )
+  return (
+    <div className={HeaderStyles.bannerDiv}>
+      <Container fluid className={HeaderStyles.bannerCon}>
+        <Row className={HeaderStyles.bannerRow}>
+          <Col lg={6} className={HeaderStyles.headerCol}>
+            <h1 className={HeaderStyles.heading1}> to the edges!</h1>
+            <h3 className={HeaderStyles.heading3}>
+              Everything to you need about climbing.
+            </h3>
+            <h5 className={HeaderStyles.heading5}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis, aliquam quod! Laudantium nemo consequuntur amet?
+            </h5>
+          </Col>
+          <Col lg={6} className={HeaderStyles.showcaseCol}>
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                backgroundColor: "aqua",
+              }}
+            ></div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 }
 
 export default Banner
--- src/components/header.js
+++ src/components/header.js
@@ -9,78 +9,62 @@ import {
   NavItem,
   NavLink,
 } from "reactstrap"
-import HeaderStyles from "../components/header.module.scss"
-import "../components/header.module.scss"
+import  headerStyles from "../components/header.module.scss"
 import "bootstrap/dist/css/bootstrap.min.css"
 import logo from "../images/header/logo.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import {
   faLinkedin,
   faFacebookSquare,
-  faInstagram
-} from '@fortawesome/free-brands-svg-icons';
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   const toggle = () => setIsOpen(!isOpen)
 
-  // const navLinks = [
-  //   { to: "/", className:"navlinks", text: "Home" },
-  //   { to: "/products", className:"navlinks", text: "Products" },
-  //   { to: "/about", className:"navlinks", text: "About Us" },
-  //   { to: "/contact", className:"navlinks", text: "Contact" },
-
-  // ];
-  // const createNavLinks = (to, className, text) => <Link to={to} className={className}>{text}</Link>
-    
-      
-    
-      
-  
   
 
-  console.log(HeaderStyles.navLinks);
+
 
   return (
     <div>
-      <Navbar className={HeaderStyles.navStyle} mr-auto expand="md" >
+      <Navbar className={headerStyles.navStyle} mr-auto expand="md">
         <NavbarBrand href="/">
-          <img src={logo} alt="Logo" className={HeaderStyles.logo}></img>
+          <img src={logo} alt="Logo" className={headerStyles.logo}></img>
         </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav navbar className={HeaderStyles.navbarNav}>
-            {/* <NavItem>
-              {navLinks.map((n)=> createNavLinks(n.to,n.className,n.text))}
-            </NavItem> */}
-            
+          <Nav navbar className={headerStyles.navbarNav}>
             <NavItem>
-              <Link className={HeaderStyles.navlinks} to="/">
+              <Link className={headerStyles.navlinks} to="/">
                 Home
               </Link>
-              <Link className={HeaderStyles.navlinks} to="/products">
+              <Link className={headerStyles.navlinks} to="/products">
                 Products
               </Link>
-              <Link className={HeaderStyles.navlinks} to="/about">
+              <Link className={headerStyles.navlinks} to="/about">
                 About
               </Link>
-              <Link className={HeaderStyles.navlinks} to="/contact">
+              <Link className={headerStyles.navlinks} to="/contact">
                 Contact
               </Link>
-            </NavItem>
-            <NavItem className={HeaderStyles.socialBar}>
-              <NavLink className={HeaderStyles.navlinks}>
+            </NavItem> 
+            
+             <NavItem className={headerStyles.socialBar}>
+              <NavLink className={headerStyles.navlinks}>
                 <FontAwesomeIcon icon={faFacebookSquare} />
               </NavLink>
-              <NavLink className={HeaderStyles.navlinks}>
+              <NavLink className={headerStyles.navlinks}>
               <FontAwesomeIcon icon={faLinkedin} />
               </NavLink>
-              <NavLink className={HeaderStyles.navlinks}>
+              <NavLink className={headerStyles.navlinks}>
               <FontAwesomeIcon icon={faInstagram} />
               </NavLink>
             </NavItem>
+            
           </Nav>
         </Collapse>
       </Navbar>
